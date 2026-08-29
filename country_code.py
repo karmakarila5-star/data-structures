3f2283a643ad6676b0f4a24d314258261add00f2
karmakarila5-star <karmakarila5@gmail.com>
2026-08-29 13:38:17 +0530

    dictionaries

---
+++ country_code.py
@@ -0,0 +1,5 @@
+country_code = {'india' : '0091','austrialia' : '0025','nepal' : '00977'}
+print("country code for india -")
+print(country_code.get('india', 'not found'))
+print("country code for japan -")
+print(country_code.get('japan', 'not found'))
